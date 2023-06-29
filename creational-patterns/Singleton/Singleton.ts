e7c139a125951089f4bda0b6267022e9e687e05f
Sofya Tuymedova <stuymedova@gmail.com>
2023-06-29 13:33:13 +0300

    Cleanup

--- creational-patterns/Singleton/Singleton.ts
+++ creational-patterns/Singleton/Singleton.ts
@@ -4,18 +4,6 @@
  * Ensures a class has only one instance and provides a
  * global point of access to it.
  * 
- * It's important for some classes to have exactly one
- * instance. Although there can be many printers in a
- * system, there should be only one printer spooler. There
- * should be only one file system and one window manager.
- * A digital filter will have one A/D converter. An
- * accounting system will be dedicated to serving one
- * company.
- * 
- * The Singleton class ensures that no other instance of
- * the object can be created and does so by intercepting
- * requests to create new objects.
- * 
  * Below the structure of one possible implementation of
  * the pattern Singleton.
  */
--- structural-patterns/Decorator/Decorator.ts
+++ structural-patterns/Decorator/Decorator.ts
@@ -1,27 +1,9 @@
 /**
  * Decorator (Wrapper)
  * 
- * Attach additional responsibilities to an object
- * dynamically. Decorators provide a flexible alternative
- * to subclassing for extending functionality.
- * 
- * One way to add responsibilities is with inheritance.
- * Inheriting a border from another class puts a border
- * around every subclass instance. This is inflexible,
- * however, because the choice of border is made
- * statically. A client can't control how and when to
- * decorate the component with a border.
- * 
- * A more flexible approach is to enclose the component in
- * another object that adds the border. The enclosing
- * object is called a decorator. The decorator conforms to
- * the interface of the component it decorates so that its
- * presence is transparent to the component's clients. The
- * decorator forwards requests to the component and may
- * perform additional actions (such as drawing a border)
- * before or after forwarding. Transparency lets you nest
- * decorators recursively, thereby allowing an unlimited
- * number of added responsibilities.
+ * Attaches additional responsibilities to a given
+ * object dynamically by enclosing (wrapping) said object
+ * within itself.
  * 
  * The following object diagram shows how to compose a
  * TextView object with BorderDecorator and ScrollDecorator
@@ -34,9 +16,8 @@
  *  ╰──────────────────╯         │ component      ●─┼────────>├──────────────────┤
  *                               ╰──────────────────╯         │                  │
  *                                                            ╰──────────────────╯
- * 
  * Below the structure of one possible implementation of
- * the pattern Observer.
+ * the pattern Decorator.
  */
 
 /**
@@ -73,7 +54,7 @@ class Decorator implements Component {
 }
 
 /**
- * ConcreteDecorators adds responsibilities to the
+ * Concrete Decorators add responsibilities to the
  * component.
  */
 class ConcreteDecoratorA extends Decorator {
